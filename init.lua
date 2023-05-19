52df1a3e61bfc377d8ce0993a30c70216bf95f1d
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2023-05-19 16:02:44 +0300

    updated dotfiles

--- init.lua
+++ init.lua
@@ -1,14 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 vim.cmd([[let g:python3_host_prog = '/home/tudor/.mamba/bin/python3']])
 vim.cmd([[
-  if $CONDA_PREFIX ==? '/home/tudor/.mamba'
-      if $VIRTUAL_ENV ==? ''
-        let g:current_python_path=$CONDA_PYTHON_EXE
-    else
-        let g:current_python_path=$VIRTUAL_ENV.'/bin/python'
-    endif
+    if $VIRTUAL_ENV ==? ''
+      let g:current_python_path=$CONDA_PYTHON_EXE
   else
-    let g:current_python_path=$CONDA_PREFIX.'/bin/python'
+      let g:current_python_path=$VIRTUAL_ENV.'/bin/python'
   endif
 ]])
 require("config.lazy")
