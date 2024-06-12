9dc94907ff39f5d18df4e98f64edfb5bb0c3d93d
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2024-06-12 18:53:47 +0300

    updated dotfiles

--- lua/plugins/blender.lua
+++ lua/plugins/blender.lua
@@ -1,7 +1,32 @@
 return {
   "b0o/blender.nvim",
   config = function()
-    require("blender").setup()
+    require("blender").setup({
+      profiles = { --                 Profile[]?       list of blender profiles
+        {
+          name = "Blender 3.6.12", --        string           profile name, must be unique
+          cmd = "/home/tudor/.blender_builds/stable/blender-3.6.12-lts.626a6b1c6799/blender", --         string|string[]  command to run Blender
+        },
+        {
+          name = "Blender 4.1.1", --        string           profile name, must be unique
+          cmd = "/home/tudor/.blender_builds/stable/blender-4.1.1-stable.e1743a0317bc/blender", --         string|string[]  command to run Blender
+        },
+        {
+          name = "Flatpak Blender 4.1.1", --        string           profile name, must be unique
+          cmd = "/var/lib/flatpak/exports/bin/org.blender.Blender", --         string|string[]  command to run Blender
+        },
+      },
+      dap = { --                      DapConfig?       DAP configuration
+        enabled = true, --            boolean?         whether to enable DAP (can be overridden per profile)
+      },
+      notify = { --                   NotifyConfig?    notification configuration
+        enabled = true, --            boolean?         whether to enable notifications
+        verbosity = "INFO", --        'TRACE'|'DEBUG'|'INFO'|'WARN'|'ERROR'|'OFF'|vim.log.level?  log level for notifications
+      },
+      watch = { --                    WatchConfig?     file watcher configuration
+        enabled = true, --            boolean?         whether to watch the add-on directory for changes (can be overridden per profile)
+      },
+    })
   end,
   dependencies = {
     "MunifTanjim/nui.nvim",
