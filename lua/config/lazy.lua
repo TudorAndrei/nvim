009704dfbe4a8bdec6059cdde4c805ff60f9a6f7
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2024-07-08 11:40:32 +0300

    updated dotfiles

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,6 +11,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- { import = "lazyvim.plugins.extras.editor.leap" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.util.startuptime" },
