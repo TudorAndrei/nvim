d107dbe80595528d6f20033715a346bd103d32b9
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2024-08-29 10:34:31 +0300

    updated dotfiles

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- { import = "lazyvim.plugins.extras.editor.leap" },
     -- { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot-chat" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.util.startuptime" },
--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -1,43 +1,43 @@
 return {
-  "nomnivore/ollama.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-
-  -- All the user commands added by the plugin
-  cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
-
-  -- Sample keybind for prompting. Note that the <c-u> is important for selections to work properly.
-  keys = {
-    {
-      "<leader>oo",
-      ":<c-u>lua require('ollama').prompt()<cr>",
-      desc = "ollama prompt",
-      mode = { "n", "v" },
-    },
-  },
-
-  ---@type Ollama.Config
-  opts = {
-    model = "openhermes2.5-mistral:latest",
-    prompts = {
-      Sample_Prompt = {
-        prompt = "Optimize this code $buf",
-        input_label = "> ",
-        model = "openhermes2.5-mistral:latest",
-        action = "display",
-      },
-    },
-  },
-  {
-    "dustinblackman/oatmeal.nvim",
-    cmd = { "Oatmeal" },
-    keys = {
-      { "<leader>om", mode = "n", desc = "Start Oatmeal session" },
-    },
-    opts = {
-      backend = "ollama",
-      model = "llama3:8b",
-    },
-  },
+  --   "nomnivore/ollama.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --   },
+  --
+  --   -- All the user commands added by the plugin
+  --   cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
+  --
+  --   -- Sample keybind for prompting. Note that the <c-u> is important for selections to work properly.
+  --   keys = {
+  --     {
+  --       "<leader>oo",
+  --       ":<c-u>lua require('ollama').prompt()<cr>",
+  --       desc = "ollama prompt",
+  --       mode = { "n", "v" },
+  --     },
+  --   },
+  --
+  --   ---@type Ollama.Config
+  --   opts = {
+  --     model = "openhermes2.5-mistral:latest",
+  --     prompts = {
+  --       Sample_Prompt = {
+  --         prompt = "Optimize this code $buf",
+  --         input_label = "> ",
+  --         model = "openhermes2.5-mistral:latest",
+  --         action = "display",
+  --       },
+  --     },
+  --   },
+  --   {
+  --     "dustinblackman/oatmeal.nvim",
+  --     cmd = { "Oatmeal" },
+  --     keys = {
+  --       { "<leader>om", mode = "n", desc = "Start Oatmeal session" },
+  --     },
+  --     opts = {
+  --       backend = "ollama",
+  --       model = "llama3:8b",
+  --     },
+  --   },
 }
